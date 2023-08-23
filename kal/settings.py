644e55ef21d3fca71fcf555a353fe67779c9eebe
Rizwan <rizwanshaik5639@gmail.com>
2023-08-23 23:52:53 +0530

    commit

--- kal/settings.py
+++ kal/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@r0phnlsg(++tb4*&vng$%41be8v%t-^b$05j%a7^4aif5syrn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['13.233.247.17','localhost','0.0.0.0']
 
